99be14916f3bb91c7a080cc5a115c21a64ef805f
Julia Poskannaya <yulya.poskannaya@gmail.com>
2021-11-10 22:58:02 +0300

    Edit key prop

--- src/components/employees-list/EmployeesList.js
+++ src/components/employees-list/EmployeesList.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/EmployeesListItem";
 
 const EmployeesList = ({data}) => {
     const elements = data.map((item) => {
-        const {id, ...itemProps} =
+        const {id, ...itemProps} = item;
         return <EmployeesListItem key={id} {...itemProps}/> //name={item.name} salary={item.sallary} increase={item.increase}
     })
     return (
