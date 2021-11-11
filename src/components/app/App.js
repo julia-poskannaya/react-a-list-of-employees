d617b747eab2901c061d508d105de6f9cebce183
Julia Poskannaya <yulya.poskannaya@gmail.com>
2021-11-11 23:10:16 +0300

    Adding a mark to rise and like

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,9 +8,9 @@ import EmployeesAddForm from "../employees-add-form/EmployeesAddForm";
 
 const App = () => {
     const data = [
-        {name: 'John D.', salary: 1000, increase: true, id: 1},
-        {name: 'Rose M.', salary: 5000, increase: false, id: 2},
-        {name: 'Stephen T.', salary: 9000, increase: false, id: 3},
+        {name: 'John D.', salary: 1000, id: 1},
+        {name: 'Rose M.', salary: 5000, id: 2},
+        {name: 'Stephen T.', salary: 9000, id: 3},
     ]
     return (
         <div className = "app">
--- src/components/employees-list-item/EmployeesListItem.js
+++ src/components/employees-list-item/EmployeesListItem.js
@@ -1,15 +1,35 @@
-const EmployeesListItem = ({name, salary, increase}) => {
+import { useState } from "react";
+
+const EmployeesListItem = ({name, salary}) => {
+    const [increaseValue, setIncreaseValue] = useState(false);
+    const [riseValue, setRiseValue] = useState(false);
+
     let classNames = "list-group-item d-flex justify-content-between";
-    if(increase){
+    if(increaseValue){
         classNames += ' increase';
     }
+
+    if(riseValue){
+        classNames += ' like';
+    }
+    
+    function onIncrease(){
+        setIncreaseValue(increaseValue => !increaseValue);
+    }
+
+    function onRise(){
+        setRiseValue(riseValue => !riseValue);
+    }
+
     return (
         <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
+            <span className="list-group-item-label" onClick={onRise}>{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
             <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
+                <button 
+                type="button" 
+                className="btn-cookie btn-sm "
+                onClick={onIncrease}>
                     <i className="fas fa-cookie"></i>
                 </button>
 
