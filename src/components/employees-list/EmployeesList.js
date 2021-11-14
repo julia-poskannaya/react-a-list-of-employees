b7d2404ee047366b0b55a224b74ce7eeb9871025
Julia Poskannaya <yulya.poskannaya@gmail.com>
2021-11-14 18:46:21 +0300

    Edit app.js, employees list

--- src/components/employees-list/EmployeesList.js
+++ src/components/employees-list/EmployeesList.js
@@ -1,9 +1,13 @@
 import EmployeesListItem from "../employees-list-item/EmployeesListItem";
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onToggleIncrease, onToggleRise}) => {
     const elements = data.map((item) => {
         const {id, ...itemProps} = item;
-        return <EmployeesListItem key={id} {...itemProps}/> //name={item.name} salary={item.sallary} increase={item.increase}
+        return <EmployeesListItem 
+        key={id} 
+        {...itemProps} //name={item.name} salary={item.sallary} increase={item.increase}
+        onToggleIncrease={() => onToggleIncrease(id)}
+        onRise={() => onToggleRise(id)}/> 
     })
     return (
         <ul className="app-list list-group">
